365e49107d8238ecba5150c4b732ccad881db694
Sam-Lemon <theatrewhisper@gmail.com>
2024-04-12 18:39:58 -0700

    finished Problems 1, and 8.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 //All questions should be printed to your Browser's console using console.log()
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
     a. Programmatically subtract the value of the first element in the array from the value in the last element of the array.
@@ -8,89 +10,51 @@
     b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
     c. Use a loop to iterate through the array and calculate the average age.
 */
-
 // Step 1
 console.log("Step 1");
-let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93]; //create the array
 console.log(ages);
+//ages.push(100); //Part B: uncomment
 
 //Part A:
 let x = ages.slice(0, 1); //stores the first element of an array. index 0, only taking 1 element
 console.log("x = " + x);
-let y = ages[ages.length - 1]; //stores the last element of an array
+let y = ages[ages.length - 1]; //stores the last element of an array. 
 console.log("y = " + y);
-//these two methods (idk if that's the right word) store the values, not return them.
-//this makes sure that they stay in the array and are not pulled out.
+//these two methods (idk if that's the right word) store the values, not return them, this makes sure that they stay in the array and are not pulled out.
 
 function subElements(x, y) {
-    return y - x; //stores the value remaining 
-} //create function to do the subtraction. parameters x and y, return to give a value
+    return y - x; //returns the value remaining 
+} //function that takes two parameters (x, y) and returns the value of y - x
 console.log("Part A: " + subElements(x, y)); // console logged the function
 
-ages.push(45); //adds the new element to the end of the array
-console.log(ages);
-
-console.log(subElements(x, y));
-
-
-
-/*
-let x = ages.shift(); //pulls the first element of the array
-let y = ages.pop(); //pulls the last element of the array
-function subElements(x, y) {
-    return y - x;
-} //create function to do the subtraction. parameters x and y, return to give a value
-console.log("Step a: " + subElements(x, y)); // console logged the function
-*/
-
-//Part B: 
-
-//ages.push(45); //pushes a new element into the last position of the array
-
-/*
-ages.push(45);
-console.log(ages);
-let x = ages.shift();
-let y = ages.pop();
-function subElements(x, y) {
-    return y - x;
-}
-console.log("Step b: " + subElements(x, y));
-*/
-
-
 //Part C:
-/*
 let sum = 0;  //I don't know why sum needs to equal 0
 for (let i = 0; i < ages.length; i++) { 
     sum += ages[i];
 }
 let average = sum / ages.length;
 console.log("Step c: " + average); 
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 2. Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
     a. Use a loop to iterate through the array and calculate the average number of letters per name.
     b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
 */
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
-    
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
 //3. How do you access the last element of any array?
-/*
-Array.length - 1
-ex: console.log(winningLotteryNums[winningLotteryNums.length - 1])
-*/
+//array[array.length -1]
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 //4. How do you access the first element of any array?
-/*
-Array.shift()
-ex: let t = array.shift();
-*/
+//array[0]; 
+//array.slice(0, 1) //this extracts the 0 index element (first element) and only that one
+//array.shift() //this removes the first element and changes the length of the array
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /*5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array 
 and add the length of each name to the nameLengths array.
 For example:
@@ -105,22 +69,47 @@ let nameLengths = [5, 3, 4];             // create a new array*/
 /*7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. 
 (i.e. if I pass in 'Hello' and 3, I would expect the function to return 'HelloHelloHello').*/
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 /*8. Write a function that takes two parameters, firstName and lastName, and returns a full name. 
 The full name should be the first and the last name separated by a space.*/
+function fullName(firstName, lastName) {
+    console.log(firstName + " " + lastName);
+}
+fullName ('Sam', 'Lemon'); //returns Sam Lemon
+//long version- two parameters and action within curly braces, need to add in the space between names and pass in names separately 
 
 
+const createFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+console.log(createFullName('Sam', 'Lemon')); //returns Sam Lemon
+//arrow function, shorter version of a function, still two parameters, used template literals and can call names within the console.log
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 //9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]; //create the array
 
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 //10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
 
-//11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
-//12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//11. Write a function that takes two arrays of numbers and returns true if the average of the elements 
+//in the first array is greater than the average of the elements in the second array.
+let array1 = [10, 11, 12, 13, 14, 15];
+let array2 = [20, 21, 22, 23, 24, 25];
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside 
+//and if moneyInPocket is greater than 10.50.
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 //13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
-
-
 
 
 
